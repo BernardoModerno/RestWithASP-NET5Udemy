c44c40b2b3e9c5e9378b223a5a5b07c13756b43d
BernardoModerno <bernardo.moderno@gmail.com>
2021-01-04 20:16:05 -0300

    Verbos Implementando as Classes Controller e Model

---
+++ RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
