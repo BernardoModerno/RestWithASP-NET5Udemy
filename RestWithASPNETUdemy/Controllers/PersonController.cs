c4c1c546715a45b536807dabdfc4cd6cf136898e
BernardoModerno <bernardo.moderno@gmail.com>
2021-01-06 19:32:15 -0300

    Controller Person

--- RestWithASPNETUdemy/Controllers/PersonController.cs
+++ RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -1,29 +1,72 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services;
 
 namespace RestWithASPNETUdemy.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class PersonController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class PersonController : ControllerBase
+  {
+
+    private readonly ILogger<PersonController> _logger;
+
+    // Declaration of the service used
+    private IPersonService _personService;
+
+    // Injection of an instance of IPersonService
+    // when creating an instance of PersonController
+    public PersonController(ILogger<PersonController> logger, IPersonService personService)
     {
-
-        private readonly ILogger<PersonController> _logger;
-        public PersonController(ILogger<PersonController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Sum(string firstNumber, string secondNumber)
-        {
-
-            return BadRequest("Invalid Input");
-        }
-
+      _logger = logger;
+      _personService = personService;
     }
+
+    // Maps GET requests to https://localhost:{port}/api/person
+    // Get no parameters for FindAll -> Search All
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return Ok(_personService.FindAll());
+    }
+
+    // Maps GET requests to https://localhost:{port}/api/person/{id}
+    // receiving an ID as in the Request Path
+    // Get with parameters for FindById -> Search by ID
+    [HttpGet("{id}")]
+    public IActionResult Get(long id)
+    {
+      var person = _personService.FindByID(id);
+      if (person == null) return NotFound();
+      return Ok(person);
+    }
+
+    // Maps POST requests to https://localhost:{port}/api/person/
+    // [FromBody] consumes the JSON object sent in the request body
+    [HttpPost]
+    public IActionResult Post([FromBody] Person person)
+    {
+      if (person == null) return BadRequest();
+      return Ok(_personService.Create(person));
+    }
+
+    // Maps PUT requests to https://localhost:{port}/api/person/
+    // [FromBody] consumes the JSON object sent in the request body
+    [HttpPut]
+    public IActionResult Put([FromBody] Person person)
+    {
+      if (person == null) return BadRequest();
+      return Ok(_personService.Update(person));
+    }
+
+    // Maps DELETE requests to https://localhost:{port}/api/person/{id}
+    // receiving an ID as in the Request Path
+    [HttpDelete("{id}")]
+    public IActionResult Delete(long id)
+    {
+      _personService.Delete(id);
+      return NoContent();
+    }
+  }
 }
